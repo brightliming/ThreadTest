f83ccadb5693d2038c0440a7f9dd1c81df349c63
bright <limingbright@yahoo.com>
2016-09-07 16:13:47 +0800

    thread test

---
+++ src/main/java/com/ehualu/thread/DeadLockDemo.java
@@ -0,0 +1,43 @@
+package com.ehualu.thread;
+
+/**
+ * Created by bright on 16-9-7.
+ */
+public class DeadLockDemo {
+    private static String A = "A";
+    private static String B = "B";
+
+    public static void main(String[] args){
+        deadLock();
+    }
+
+    public static void deadLock(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (A){
+                    try {
+                        Thread.currentThread().sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized(B){
+                        System.out.println("1");
+                    }
+                }
+            }
+        }).start();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (B){
+                    synchronized (A){
+                        System.out.println("2");
+                    }
+                }
+            }
+        }).start();
+    }
+}
