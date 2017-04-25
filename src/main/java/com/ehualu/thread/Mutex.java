ef4af400698684140a8b98225c04bc2eb367ace9
limingbright <limingbright@yahoo.com>
2017-04-25 22:56:49 +0800

    Mutex TwinLock

---
+++ src/main/java/com/ehualu/thread/Mutex.java
@@ -0,0 +1,65 @@
+package com.ehualu.thread;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Created by Administrator on 2017/4/24.
+ */
+public class Mutex implements Lock {
+
+    private static class Sync extends AbstractQueuedSynchronizer{
+        protected boolean isHeldExclusively(){
+            return getState() == 1;
+        }
+
+        public boolean tryAcquire(int acquires){
+            if(compareAndSetState(0,1)){
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        protected boolean tryRelease(int releases){
+            if(getState()==0){
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(Thread.currentThread());
+            setState(0);
+            return true;
+        }
+
+        Condition newCondition(){
+            return new ConditionObject();
+        }
+    }
+
+    private final Sync sync = new Sync();
+
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1,unit.toNanos(time));
+    }
+
+    public void unlock() {
+        sync.release(1);
+    }
+
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+}
